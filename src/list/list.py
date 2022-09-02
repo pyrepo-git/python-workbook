f244ce129e870134477f855c0febad8ab9e1918c
pyrepo-git <49498725+pyrepo-git@users.noreply.github.com>
2022-09-02 19:10:52 +0300

    Update list.py

--- src/list/list.py
+++ src/list/list.py
@@ -6,15 +6,39 @@ from typing import List
 """
 Python data structures : list.
 """
+values: List[int] = []
 
 
-def lest_exercise1(arg: Optional[Any]) -> List[int]:
+def lest_exercise1() -> None:
     """
     (Solved, 22 Lines)
-        Write a program that reads integers from the user and stores them in a list. Your
-        program should continue reading values until the user enters 0. Then it should display
-        all of the values entered by the user (except for the 0) in ascending order, with one
-        value appearing on each line. Use either the sort method or the sorted function
+        Write a program that reads integers from the user and stores them in
+        a list. Your program should continue reading values until the user
+        enters 0. Then it should display all of the values entered by the user
+        (except for the 0) in ascending order, with one value appearing on
+        each line. Use either the sort method or the sorted function.
         to sort the list.
     """
-    pass
+
+    # Read values from the user and store them in a list until a 0 is entered
+    line = input("Enter a number (0) line to quit: ")
+    while line != "0":
+        num = int(line)
+        values.append(num)
+
+        line = input("Enter a number (0) line to quit: ")
+
+    # Sort values into ascending order
+    values.sort()
+
+    # Display values
+    for v in values:
+        print(v)
+
+
+def main() -> None:
+    lest_exercise1()
+
+
+if __name__ == "__main__":
+    main()
