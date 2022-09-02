ce42dff96fecbde4a2785d18d78d85dc2e539024
pyrepo-git <49498725+pyrepo-git@users.noreply.github.com>
2022-09-02 17:39:44 +0300

    Update list.py

--- src/list/list.py
+++ src/list/list.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python 3
 # -*- coding: utf-8 -*-
 
+from typing import List
+
 """
 Python data structures : list.
 """
+
+
+def lest_exercise1(arg: Optional[Any]) -> List[int]:
+    """
+    (Solved, 22 Lines)
+        Write a program that reads integers from the user and stores them in a list. Your
+        program should continue reading values until the user enters 0. Then it should display
+        all of the values entered by the user (except for the 0) in ascending order, with one
+        value appearing on each line. Use either the sort method or the sorted function
+        to sort the list.
+    """
+    pass
